57a8820e162705fcff862d1b83dad5c43014a7e6
j0n-a <92008991+j0n-a@users.noreply.github.com>
2025-02-21 11:12:30 -0800

    Added `check_paths()`

--- Child_GoNoGo_task.py
+++ Child_GoNoGo_task.py
@@ -5,3 +5,7 @@ from helper import generate_trials
 import time
 
 # this task could be basically the same as the left right task but where they only go on green and not yellow
+
+##### MAKE SURE ALL THE PIECES ARE IN THE RIGHT PLACE #####
+current_directory = os.path.dirname(os.path.abspath(__file__))
+check_paths(current_directory)
